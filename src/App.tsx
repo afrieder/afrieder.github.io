41a5554686152d6bf8e034bd4183e3f4b222f70d
Alex Frieder <alex.frieder@gmail.com>
2018-05-08 22:27:32 -0700

    Made things work right

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+class App extends React.Component<{}, {}> {
+  render(): JSX.Element {
+    return (
+      <div>Hello, World!</div>
+    );
+  }
+};
+
+export { App };
